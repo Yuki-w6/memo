c973353b0104e09566f4b7909ebb0ddecf5b7cab
Yuki-w6 <m1610600@gmail.com>
2023-10-29 19:24:08 +0900

    edit dokcer

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -12,10 +12,11 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      Editfff
+      <code>components/HelloWorld.vue</code> sssto test HMR
     </p>
   </div>
+  aaaaaaaaaad
 
   <p>
     Check out
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath } from "url";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": path.resolve(__dirname, 'src'),
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    hmr: {
+      host: 'localhost',
+      port: 5173
     }
   }
 })
